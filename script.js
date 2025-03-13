894027844f221e423cf74e80212de785ed649c6c
Lorenzo Giambellini <lorenzo.giambellini@icloud.com>
2025-03-13 11:14:15 +0100

    flexible squares in js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const container = document.querySelector(".container");
 container.className = "container";
-for(let i = 0; i<16*16; i++){
-    const square = document.createElement("div");
-    square.className = "square";
-    container.appendChild(square);
+let squareSize = 16;
+function createGrid(x){
+    for(let i = 0; i<x*x; i++){
+        let square = document.createElement("div");
+        square.className = "square";
+        container.appendChild(square);
+    } 
 }
-
+createGrid(squareSize);
 
 document.querySelectorAll(".square").forEach(square => {
     square.addEventListener("mouseenter", () => {
@@ -16,4 +19,15 @@ document.querySelectorAll(".square").forEach(square => {
 
 function getRandomColor() {
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+const sizeBtn = document.querySelector(".size-btn");
+const containerSize = 960;
+sizeBtn.addEventListener("click", ()=> {
+    let n = prompt("insert a number");
+    document.documentElement.style.setProperty("--square-size", `${containerSize / n}px`);
+    resize(n);
+});
+function resize(x) {
+    container.innerHTML = ""; // Svuota il contenitore prima di ricreare la griglia
+    createGrid(x);
 }
